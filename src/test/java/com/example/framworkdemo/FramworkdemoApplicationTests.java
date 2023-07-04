e18247ce75b1aa5aa652476a610b044cf583dcbe
pandeng <panda_1214@163.com>
2023-07-04 22:28:50 +0800

    框架demo

---
+++ src/test/java/com/example/framworkdemo/FramworkdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.framworkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FramworkdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
