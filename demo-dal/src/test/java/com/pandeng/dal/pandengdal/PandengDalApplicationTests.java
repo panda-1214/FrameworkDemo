73a932717b9465d712a5ab4a6fb8bf866b3a8f56
unknown <panda_1214@163.com>
2023-07-07 17:39:36 +0800

    架构

---
+++ demo-dal/src/test/java/com/pandeng/dal/pandengdal/PandengDalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandeng.dal.pandengdal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandengDalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ demo-web/src/test/java/com/pandeng/web/pandengweb/PandengWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandeng.web.pandengweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandengWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
